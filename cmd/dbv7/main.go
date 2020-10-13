37280639d6428ee3d078d9d1e6f5d1c2f93127d0
Tin Nguyen <nguyentrongtin89@gmail.com>
2020-10-14 02:47:06 +0700

    fix organization_linkedin_numerical_urls array

--- cmd/dbv7/main.go
+++ cmd/dbv7/main.go
@@ -29,21 +29,21 @@ type CsvRecord struct {
 
 //json
 type _Source struct {
-	PersonName                        string `json:"person_name"`
-	PersonFirstNameUnanalyzed         string `json:"person_first_name_unanalyzed"`
-	PersonLastNameUnanalyzed          string `json:"person_last_name_unanalyzed"`
-	PersonNameUnanalyzedDowncase      string `json:"person_name_unanalyzed_downcase"`
-	PersonEmailStatusCd               string `json:"person_email_status_cd"`
-	PersonExtrapolatedEmailConfidence string `json:"person_extrapolated_email_confidence"`
-	PersonExtrapolatedEmail           string `json:"person_extrapolated_email"`
-	PersonEmail                       string `json:"person_email"`
-	PersonLinkedinUrl                 string `json:"person_linkedin_url"`
-	PersonPhone                       string `json:"person_phone"`
-	PersonLocalCountry                string `json:"person_location_country"`
-	SanitizedOrganizationName         string `json:"sanitized_organization_name_unanalyzed"`
-	OrganizationName                  string `json:"organization_name"`
-	OrganizationLinkedinNumericalUrls string `json:"organization_linkedin_numerical_urls"`
-	Origin                            string `json:"origin"`
+	PersonName                        string   `json:"person_name"`
+	PersonFirstNameUnanalyzed         string   `json:"person_first_name_unanalyzed"`
+	PersonLastNameUnanalyzed          string   `json:"person_last_name_unanalyzed"`
+	PersonNameUnanalyzedDowncase      string   `json:"person_name_unanalyzed_downcase"`
+	PersonEmailStatusCd               string   `json:"person_email_status_cd"`
+	PersonExtrapolatedEmailConfidence string   `json:"person_extrapolated_email_confidence"`
+	PersonExtrapolatedEmail           string   `json:"person_extrapolated_email"`
+	PersonEmail                       string   `json:"person_email"`
+	PersonLinkedinUrl                 string   `json:"person_linkedin_url"`
+	PersonPhone                       string   `json:"person_phone"`
+	PersonLocalCountry                string   `json:"person_location_country"`
+	SanitizedOrganizationName         string   `json:"sanitized_organization_name_unanalyzed"`
+	OrganizationName                  string   `json:"organization_name"`
+	OrganizationLinkedinNumericalUrls []string `json:"organization_linkedin_numerical_urls"`
+	Origin                            string   `json:"origin"`
 }
 
 func (s _Source) IsNotValid() bool {
