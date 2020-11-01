2754e767e7671ac01ba0f7cb65d402d42d4865a7
Tin Nguyen <nguyentrongtin89@gmail.com>
2020-11-01 22:53:55 +0700

    Add method get 3 highest

--- internal/model/csvstruct/emailpatern.go
+++ internal/model/csvstruct/emailpatern.go
@@ -51,3 +51,98 @@ func NewEmailPatternFromLine(line string) (*EmailPattern, error) {
 
 	return emailPattern, nil
 }
+
+func (emailPattern EmailPattern) First3HighestScorePatterns() (score []float64, pattern []string){
+	max1 := 0.0
+	idx1 := -1
+
+	max2 := 0.0
+	idx2 := -1
+
+	max3 := 0.0
+	idx3 := -1
+
+	for idx, score := range emailPattern.Score {
+		if score > max1 {
+			max3 = max2
+			idx3 = idx2
+
+			max2 = max1
+			idx2 = idx1
+
+			max1 = score
+			idx1 = idx
+
+			continue
+		}
+
+		if score > max2 {
+			max3 = max2
+			idx3 = idx2
+
+			max2 = score
+			idx2 = idx
+
+			continue
+		}
+
+		if score > max3 {
+			max3 = score
+			idx3 = idx
+
+			continue
+		}
+	}
+
+	score = make([]float64, 3)
+	pattern = make([]string, 3)
+
+	if idx1 == -1 {
+		for i:=0; i<3;i++ {
+			score[i] = emailPattern.Score[i]
+			pattern[i] = emailPattern.Pattern[i]
+		}
+		return
+	} else {
+		score[0] = emailPattern.Score[idx1]
+		pattern[0] = emailPattern.Pattern[idx1]
+	}
+
+	if idx2 == -1 {
+		walk := 1
+		for i:=0; i<30;i++ {
+			if i != idx1 && emailPattern.Score[i] == 0.0{
+				score[walk] = emailPattern.Score[i]
+				pattern[walk] = emailPattern.Pattern[i]
+				walk++
+			}
+			if walk == 3 {
+				break
+			}
+		}
+		return
+	} else {
+		score[1] = emailPattern.Score[idx2]
+		pattern[1] = emailPattern.Pattern[idx2]
+	}
+
+	if idx3 == -1 {
+		walk := 2
+		for i:=0; i<30;i++ {
+			if i != idx1 && i != idx2 && emailPattern.Score[i] == 0.0{
+				score[walk] = emailPattern.Score[i]
+				pattern[walk] = emailPattern.Pattern[i]
+				walk++
+			}
+			if walk == 3 {
+				break
+			}
+		}
+		return
+	} else {
+		score[2] = emailPattern.Score[idx3]
+		pattern[2] = emailPattern.Pattern[idx3]
+	}
+
+	return
+}
--- internal/model/csvstruct/emailpattern_test.go
+++ internal/model/csvstruct/emailpattern_test.go
@@ -19,3 +19,18 @@ func TestNewEmailPatternFromLine_Wrong(t *testing.T) {
 	_, err := NewEmailPatternFromLine(line)
 	assert.Equal(t, INVALIDINPUTERR, err)
 }
+
+func TestEmailPattern_First3HighestScorePatterns(t *testing.T) {
+	line := "0.00,Initial,0.14,First,0.14,FirstL,0.00,First.L,0.00,First_L,0.00,First-L,0.00,FirstTwofirstletteroflast,0.00,LFirst,0.00,L-First,0.00,L.First,0.00,L_First,0.00,TwofirstletteroflastFirst,0.13,Last,0.01,LastF,0.00,Last.F,0.00,Last_F,0.00,Last-F,0.00,TwofirstletteroffirstLast,0.36,FLast,0.00,F-Last,0.01,F.Last,0.00,F_Last,0.00,LastTwofirstletteroffirst,0.00,LastFirst,0.00,Last.First,0.19,FirstLast,0.01,First.Last,0.00,First_Last,0.00,First-Last,0.00,Last_First,google.com,13383"
+
+	pattern, err := NewEmailPatternFromLine(line)
+	assert.Nil(t, err)
+
+	scores, patterns := pattern.First3HighestScorePatterns()
+	assert.Equal(t, 0.36, scores[0])
+	assert.Equal(t, 0.19, scores[1])
+	assert.Equal(t, 0.14, scores[2])
+	assert.Equal(t, "FLast", patterns[0])
+	assert.Equal(t, "FirstLast", patterns[1])
+	assert.Equal(t, "First", patterns[2])
+}
