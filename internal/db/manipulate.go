20eff1c856551fa81ac6a8cf30fa65eed9d4beec
Tin Nguyen <nguyentrongtin89@gmail.com>
2020-11-01 19:13:57 +0700

    Add method delete

--- internal/db/manipulate.go
+++ internal/db/manipulate.go
@@ -8,21 +8,25 @@ import (
 )
 
 var NOTFOUNDERR = errors.New("not found")
+var DOMAINEXISTEDERR = errors.New("domain name existed")
+var COMMITEDERR = errors.New("can not commit transaction")
 
 type DataService interface {
 	FindEmailPatternByDomain(domain string) ([]*emailpattern.EmailPattern, error)
 	UpdateDomainToOld(domain string) (int64, error)
+	InsertNewEmailPattern(emailPattern emailpattern.EmailPattern) (int64, error)
+	DeleteDomain(domain string) (int64, error)
 }
 
 type dataManipulator struct {
 	ctx context.Context
-	db *sql.DB
+	db  *sql.DB
 }
 
 func NewDataService(ctx context.Context, db *sql.DB) DataService {
 	return &dataManipulator{
 		ctx: ctx,
-		db: db,
+		db:  db,
 	}
 }
 
@@ -83,7 +87,77 @@ func (manipulator *dataManipulator) UpdateDomainToOld(domain string) (int64, err
 	}
 
 	if err = tx.Commit(); err != nil {
+		return 0, COMMITEDERR
+	}
+
+	return result.RowsAffected()
+}
+
+func (manipulator *dataManipulator) InsertNewEmailPattern(emailPattern emailpattern.EmailPattern) (int64, error) {
+	txOption := &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false,
+	}
+	tx, err := manipulator.db.BeginTx(manipulator.ctx, txOption)
+	if err != nil {
 		return 0, err
+	}
+
+	rowsExisted, err := tx.QueryContext(manipulator.ctx, "SELECT domain_name FROM email_pattern WHERE domain_name = ?", emailPattern.DomainName)
+	if err != nil {
+		return 0, err
+	}
+
+	if rowsExisted.Next() {
+		if err = tx.Commit(); err != nil {
+			return 0, COMMITEDERR
+		}
+
+		return 0, DOMAINEXISTEDERR
+	}
+
+	result, err := tx.ExecContext(manipulator.ctx,
+		"INSERT INTO email_pattern(score1, pattern1, score2, pattern2, score3, pattern3, domain_name, entry) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+		emailPattern.Score1,
+		emailPattern.Pattern1,
+		emailPattern.Score2,
+		emailPattern.Pattern2,
+		emailPattern.Score3,
+		emailPattern.Pattern3,
+		emailPattern.DomainName,
+		emailPattern.Entry)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return 0, COMMITEDERR
+	}
+
+	return result.RowsAffected()
+}
+
+func (manipulator *dataManipulator) DeleteDomain(domain string) (int64, error) {
+	txOption := &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+		ReadOnly:  false,
+	}
+	tx, err := manipulator.db.BeginTx(manipulator.ctx, txOption)
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := tx.ExecContext(manipulator.ctx,
+		"DELETE FROM email_pattern WHERE domain_name = ?",
+		domain)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return 0, COMMITEDERR
 	}
 
 	return result.RowsAffected()
--- internal/db/manipulate_test.go
+++ internal/db/manipulate_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
+	"parsecsv/internal/model/emailpattern"
 	"testing"
 )
 
@@ -49,5 +50,50 @@ func TestDataManipulator_UpdateDomainToOld(t *testing.T) {
 
 	_, err = dataService.FindEmailPatternByDomain("google.com")
 	assert.Equal(t, NOTFOUNDERR, err)
+}
 
+func TestDataManipulator_InsertDeleteEmailPattern(t *testing.T) {
+	ctx := context.Background()
+	connectService := NewMysqlConnector(ctx, 5, 2)
+	db, err := connectService.Connect(
+		"xxxxx",
+		"xxxxxxxx",
+		"xxxx:3306",
+		"xxx")
+	defer connectService.Disconnect()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+
+	emailPattern := emailpattern.EmailPattern{
+		Score1:     33.33,
+		Pattern1:   "First.Last",
+		Score2:     33.33,
+		Pattern2:   "Last.First",
+		Score3:     33.33,
+		Pattern3:   "Others",
+		DomainName: "oto4u.vn",
+		Entry:      100,
+	}
+	dataService := NewDataService(ctx, db)
+	insertedRows, err := dataService.InsertNewEmailPattern(emailPattern)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), insertedRows)
+
+	emails, err := dataService.FindEmailPatternByDomain("oto4u.vn")
+	assert.Nil(t, err)
+	assert.Equal(t, "oto4u.vn", emails[0].DomainName)
+	assert.Equal(t, 33.33, emails[0].Score1)
+	assert.Equal(t, "First.Last", emails[0].Pattern1)
+	assert.Equal(t, 33.33, emails[0].Score2)
+	assert.Equal(t, "Last.First", emails[0].Pattern2)
+	assert.Equal(t, 33.33, emails[0].Score3)
+	assert.Equal(t, "Others", emails[0].Pattern3)
+
+	_, err = dataService.InsertNewEmailPattern(emailPattern)
+	assert.Equal(t, DOMAINEXISTEDERR, err)
+
+	deletedRows, err := dataService.DeleteDomain("oto4u.vn")
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), deletedRows)
 }
